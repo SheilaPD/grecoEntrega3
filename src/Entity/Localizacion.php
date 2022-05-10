243ede3896f004cb919970c6abd2cceea0b5909d
Luis Ramón López <lrlopez@gmail.com>
2022-05-10 13:38:24 +0200

    Implementada entidad Localizacion

---
+++ src/Entity/Localizacion.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocalizacionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LocalizacionRepository::class)
+ */
+class Localizacion
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $codigo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $descripcion;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Localizacion::class, inversedBy="hijos")
+     */
+    private $padre;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Localizacion::class, mappedBy="padre")
+     */
+    private $hijos;
+
+    public function __construct()
+    {
+        $this->hijos = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCodigo(): ?string
+    {
+        return $this->codigo;
+    }
+
+    public function setCodigo(string $codigo): self
+    {
+        $this->codigo = $codigo;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(?string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getPadre(): ?self
+    {
+        return $this->padre;
+    }
+
+    public function setPadre(?self $padre): self
+    {
+        $this->padre = $padre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getHijos(): Collection
+    {
+        return $this->hijos;
+    }
+
+    public function addHijo(self $hijo): self
+    {
+        if (!$this->hijos->contains($hijo)) {
+            $this->hijos[] = $hijo;
+            $hijo->setPadre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeHijo(self $hijo): self
+    {
+        if ($this->hijos->removeElement($hijo)) {
+            // set the owning side to null (unless already changed)
+            if ($hijo->getPadre() === $this) {
+                $hijo->setPadre(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Repository/LocalizacionRepository.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Localizacion;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Localizacion>
+ *
+ * @method Localizacion|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Localizacion|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Localizacion[]    findAll()
+ * @method Localizacion[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LocalizacionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Localizacion::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Localizacion $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Localizacion $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    // /**
+    //  * @return Localizacion[] Returns an array of Localizacion objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Localizacion
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
