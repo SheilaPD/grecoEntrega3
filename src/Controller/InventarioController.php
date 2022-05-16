0c05920a61f544585ea1c13819c025876dba478a
Sheila Peralta <sheila.peralta.587@estudiante.iesoretania.es>
2022-05-16 22:18:09 +0200

    Implementado inventarioController

---
+++ src/Controller/InventarioController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\LocalizacionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InventarioController extends AbstractController
+{
+    /**
+     * @Route("/inventario/listar", name="listar_loc")
+     */
+    public function productoListar(LocalizacionRepository $localizacionRepository): Response
+    {
+        $localizaciones = $localizacionRepository->findOrd();
+
+        return $this->render('inventario/listarLoc.html.twig', [
+            'localizaciones' => $localizaciones
+        ]);
+    }
+}
